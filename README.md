6e3ad43d5160b89c6e92109d9e401d66c8c39645
Gabriel Correia <gabrielcorreia01012020@hotmail.com>
2022-12-11 18:15:53 -0300

    VER(0.0.1) droidcat_ctx_t is now be initialized and destroyed

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ## Installation
 
-Droidcat binaries can't run directly inside the build directory that you compiles the program in, so on, you need to install the application by issuing a `ninja install` inside the generated _build_ directory!
+Droidcat binaries can't run directly inside the build directory, so on, you need to install the application by issuing a `ninja install` inside the generated _build_ directory!
 
 > :bulb: **Tip**: You can install direct inside some system location like /usr/share/**droidcat** (HERE), but you will need the root user permissions for doing this!
 
@@ -12,6 +12,6 @@ For a better explanation for how the flow works, read the documents pages!
 
 <detail>
     <summary>:house_with_garden: A brief about droidcat!</summary>
-    All that you need for manager Android Packages into a hole application, that's makes you productively and faster enoughs for cracking and studies reverse engineering techniques inside this world! 
+    All that you need for manager Android Packages into a hole application, that makes you productively and faster enough for cracking and studies reverse engineering techniques inside this world! 
 </details>
 
--- src/main_thread.c
+++ src/main_thread.c
@@ -1,8 +1,61 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include <malloc.h>
+
+typedef int32_t i32;
+
+typedef struct user_options
+{
+  bool interactive_mode;
+  bool display_welcome;
+
+} user_options_t;
+
 typedef struct droidcat_ctx
 {
+  user_options_t* user_interact;
 
 } droidcat_ctx_t;
 
-int main(void)
+#define DROID_ALLOCATE_SAFE(type_inference, elements_count)\
+  (type_inference*)calloc(elements_count, sizeof(type_inference))
+
+i32 droidcat_init(droidcat_ctx_t* droidcat_ctx) 
 {
+  if (droidcat_ctx == NULL) return -1;
+
+  droidcat_ctx->user_interact = DROID_ALLOCATE_SAFE(user_options_t, 1);
+  if (droidcat_ctx->user_interact == NULL) return -1;
+
+  return 0;
+}
+
+i32 droidcat_deinit(droidcat_ctx_t* droidcat_ctx)
+{
+  if (droidcat_ctx == NULL) return -1;
+
+  if (droidcat_ctx->user_interact != NULL)
+  {
+    free(droidcat_ctx->user_interact);
+  }
+
+  return 0;
+}
+
+i32 main(void)
+{
+  droidcat_ctx_t* droidcat_proc_ctx = DROID_ALLOCATE_SAFE(droidcat_ctx_t, 1);
+  i32 droidcat_status = 0;
+
+  if ((droidcat_status = droidcat_init(droidcat_proc_ctx)) != 0)
+  {
+    free(droidcat_proc_ctx);
+    return droidcat_status;
+  }
+
+  droidcat_deinit(droidcat_proc_ctx);
+
+  free(droidcat_proc_ctx);
+  return 0;
+  
 }
